4f0ab7c5c7a0f38eec437803fbc7a7cdca183362
Timo Reimann <ttr314@googlemail.com>
2017-04-07 11:40:59 +0200

    Extend Marathon integration test.

--- integration/marathon_test.go
+++ integration/marathon_test.go
@@ -1,11 +1,13 @@
 package integration
 
 import (
+	"fmt"
 	"net/http"
 	"os/exec"
 	"time"
 
 	"github.com/containous/traefik/integration/try"
+	marathon "github.com/gambol99/go-marathon"
 	"github.com/go-check/check"
 	checker "github.com/vdemeester/shakers"
 )
@@ -14,6 +16,7 @@ import (
 type MarathonSuite struct{ BaseSuite }
 
 func (s *MarathonSuite) SetUpSuite(c *check.C) {
+	fmt.Println("setting up marathon compose file")
 	s.createComposeProject(c, "marathon")
 	s.composeProject.Start(c)
 
@@ -51,4 +54,34 @@ func (s *MarathonSuite) TestSimpleConfiguration(c *check.C) {
 	err = try.GetRequest("http://127.0.0.1:8000/", 500*time.Millisecond, try.StatusCodeIs(http.StatusNotFound))
 
 	c.Assert(err, checker.IsNil)
+}
+
+func (s *MarathonSuite) TestConfigurationUpdate(c *check.C) {
+	cmd := exec.Command(traefikBinary, "--configFile=fixtures/marathon/with-entrypoint.toml")
+	err := cmd.Start()
+	c.Assert(err, checker.IsNil)
+	defer cmd.Process.Kill()
+
+	marathonURL := "http://marathon:8080"
+	fmt.Printf("polling Marathon URL %s for availability\n", marathonURL)
+	// wait for marathon
+	err = try.GetRequest(fmt.Sprintf("%s/ping", marathonURL), 2*time.Minute, try.StatusCodeIs(http.StatusOK))
+	c.Assert(err, checker.IsNil)
+
+	// Prepare Marathon client.
+	config := marathon.NewDefaultConfig()
+	config.URL = marathonURL
+	client, err := marathon.NewClient(config)
+	c.Assert(err, checker.IsNil)
+
+	// Deploy test application via Marathon.
+	app := marathon.NewDockerApplication().Name("/whoami").CPU(0.1).Memory(32)
+	app.Container.Docker.Container("emilevauge/whoami")
+
+	fmt.Println("deploying test application")
+	deployID, err := client.UpdateApplication(app, false)
+	c.Assert(err, checker.IsNil)
+	c.Assert(client.WaitOnDeployment(deployID.DeploymentID, 30*time.Second), checker.IsNil)
+
+	fmt.Println("done.")
 }
